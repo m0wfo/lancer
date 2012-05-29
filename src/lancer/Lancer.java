b2d96413a8d6d65165619d685d826f8c8ab34d03
Chris Mowforth <chris@mowforth.com>
2012-05-29 17:33:39 +0100

    import.

---
+++ src/lancer/Lancer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lancer;
+
+/**
+ *
+ * @author chris
+ */
+public class Lancer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
